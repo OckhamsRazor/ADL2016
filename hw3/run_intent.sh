3d8306549eff32added2e0d4ee15f70681b26c92
Lang-Chi Yu <yl871804@gmail.com>
2016-11-30 18:59:57 +0800

    hw3 scripts param

--- hw3/run_intent.sh
+++ hw3/run_intent.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python2.7 rnn.int.eval.py -i atis.test.iob -c int.dr40.class -m int.dr40/ -o answer.txt -hi 128
+python2.7 rnn.int.eval.py -i $1 -c int.dr40.class -m int.dr40/ -o $2 -hi 128
--- hw3/run_slot.sh
+++ hw3/run_slot.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python2.7 rnn.eval.py -i atis.test.iob -c slot.128.dr20.class -o answer.txt -m slot.128.dr20 -hi 128
+python2.7 rnn.eval.py -i $1 -c slot.128.dr20.class -o $2 -m slot.128.dr20 -hi 128
